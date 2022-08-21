b64d3a66af25068fcd641a1729ad7557f0a7f813
TungLe <thanhtung12121212@gmail.com>
2022-08-22 00:38:00 +0700

    LAB08.7

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user')
 
+const MONGODB_URI = 'mongodb+srv://tunglt:dvalvuumm1ty1@cluster0.5hjpvkp.mongodb.net/shop?retryWrites=true&w=majority'
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'session'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,7 +26,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }));
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }));
 
 app.use((req, res, next) => {
     User.findById("62fcb4b968eacff4ab40627b")
@@ -38,7 +45,7 @@ app.use(errorController.get404);
 
 
 mongoose
-    .connect('mongodb+srv://tunglt:dvalvuumm1ty1@cluster0.5hjpvkp.mongodb.net/shop?retryWrites=true&w=majority')
+    .connect(MONGODB_URI)
     .then(result => {
         User.findOne().then(user => {
             if (!user) {
