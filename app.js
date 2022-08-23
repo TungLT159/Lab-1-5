b0f440b9228bfe819eade4bb255cb1bf5aa0c524
TungLe <thanhtung12121212@gmail.com>
2022-08-23 23:12:56 +0700

    LAB08.10

--- app.js
+++ app.js
@@ -29,6 +29,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }));
 
 app.use((req, res, next) => {
+    if (!req.session.user) {
+        return next()
+    }
+    User.findById(req.session.user._id)
+        .then(user => {
+            req.user = user
+            next()
+        })
+        .catch(err => console.log(err));
+})
+
+app.use((req, res, next) => {
     User.findById("62fcb4b968eacff4ab40627b")
         .then(user => {
             req.user = user
