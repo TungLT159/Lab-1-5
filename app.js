6adbe4699ed497e4f6bb98becdc67246dbd8c8b0
TungLe <thanhtung12121212@gmail.com>
2022-08-25 16:21:12 +0700

    LAB09.1

--- app.js
+++ app.js
@@ -9,16 +9,17 @@ const csrf = require('csurf');
 const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user')
+const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://tunglt:dvalvuumm1ty1@cluster0.5hjpvkp.mongodb.net/shop?retryWrites=true&w=majority'
+const MONGODB_URI =
+    'mongodb+srv://tunglt:dvalvuumm1ty1@cluster0.5hjpvkp.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
     uri: MONGODB_URI,
-    collection: 'session'
-})
-const csrfProtection = csrf()
+    collection: 'sessions'
+});
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -29,39 +30,46 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }));
-
-app.use(csrfProtection)
-app.use(flash())
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
     if (!req.session.user) {
-        return next()
+        return next();
     }
     User.findById(req.session.user._id)
         .then(user => {
-            req.user = user
-            next()
+            req.user = user;
+            next();
         })
         .catch(err => console.log(err));
-})
+});
 
 app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn
-    res.locals.csrfToken = req.csrfToken()
-    next()
-})
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(authRoutes)
+app.use(authRoutes);
 
 app.use(errorController.get404);
-
 
 mongoose
     .connect(MONGODB_URI)
     .then(result => {
-        app.listen(3000)
+        app.listen(3000);
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+        console.log(err);
+    });
