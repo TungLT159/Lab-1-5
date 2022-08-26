77a229707a9b8beb0ede3b17a31ff9c846f8ca5c
TungLe <thanhtung12121212@gmail.com>
2022-08-26 22:04:34 +0700

    LAB09.18

--- app.js
+++ app.js
@@ -42,11 +42,19 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
+app.use((req, res, next) => {
+    // throw new Error('Cannot connect')
     if (!req.session.user) {
         return next();
     }
     User.findById(req.session.user._id)
         .then(user => {
+            // throw new Error('error loading')
             if (!user) {
                 return next();
             }
@@ -54,15 +62,11 @@ app.use((req, res, next) => {
             next();
         })
         .catch(err => {
-            throw new Error(err);
+            next(new Error(err))
         });
 });
 
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
-});
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -73,7 +77,12 @@ app.get('/500', errorController.get500)
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-    res.redirect('/500')
+    // res.redirect('/500')
+    res.status(404).render('500', {
+        pageTitle: 'Server error',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn
+    });
 })
 
 mongoose
