901cd3de5cbb92949c06e7162710abbae5678df1
TungLe <thanhtung12121212@gmail.com>
2022-08-28 16:30:32 +0700

    LAB10.2

--- routes/admin.js
+++ routes/admin.js
@@ -20,7 +20,6 @@ router.post('/add-product', [
     .isString()
     .isLength({ min: 3 })
     .trim(),
-    body('imageUrl').isURL(),
     body('price').isFloat(),
     body('description')
     .isLength({ min: 5, max: 100 })
