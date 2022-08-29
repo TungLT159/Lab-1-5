54e9ec33e808ddcf7ee4bb2e46678936484f0484
TungLe <thanhtung12121212@gmail.com>
2022-08-29 22:22:47 +0700

    LAB10.22

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,15 +9,31 @@ const Order = require('../models/order');
 const ITEM_PER_PAGE = 1
 
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1
+    let totalItems
     Product
         .find()
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts
+            return Product
+                .find()
+                .skip((page - 1) * ITEM_PER_PAGE)
+                .limit(ITEM_PER_PAGE)
+        })
         .then(products => {
-            console.log(products);
             res.render('shop/product-list', {
                 prods: products,
-                pageTitle: 'All Products',
-                path: '/products'
-            });
+                pageTitle: 'Products',
+                path: '/products',
+                currentPage: page,
+                hasNextPage: ITEM_PER_PAGE * page < totalItems,
+                hasPrevPage: page > 1,
+                nextPage: page + 1,
+                prevPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEM_PER_PAGE)
+
+            })
         })
         .catch(err => {
             const error = new Error(err)
