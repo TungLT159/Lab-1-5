a041125f8e90563e461046643dd0bd6e5c491164
TungLe <thanhtung12121212@gmail.com>
2022-08-29 17:06:04 +0700

    LAB10.10

--- controllers/shop.js
+++ controllers/shop.js
@@ -150,12 +150,14 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
     const orderId = req.params.orderId
-    const invoiceName = 'invoice-' + orderId + 'pdf'
+    const invoiceName = 'invoice-' + orderId + '.pdf'
     const invoicePath = path.join('data', 'invoices', invoiceName)
     fs.readFile(invoicePath, (err, data) => {
         if (err) {
             return next(err)
         }
+        res.setHeader('Content-Type', 'application/pdf')
+        res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"')
         res.send(data)
 
     })
